5db814d91c4b2b643333fb2ab33a15e605a4482d
Brayan M <71532006+bmontiel-28@users.noreply.github.com>
2024-06-06 17:03:56 -0600

    Update Obtener la pass de usuarios de EBS.sql

--- Obtener la pass de usuarios de EBS.sql
+++ Obtener la pass de usuarios de EBS.sql
@@ -25,3 +25,24 @@ FROM
     FND_USER USR
 WHERE
     USR.USER_NAME IN ('MARTINEZJAI','MARQUEZV','DELEONV','LIMAD');
+
+
+
+
+
+-- Paquete en caso de que no exista
+CREATE OR REPLACE PACKAGE get_pwd
+AS
+   FUNCTION decrypt (KEY IN VARCHAR2, VALUE IN VARCHAR2)
+      RETURN VARCHAR2;
+END get_pwd;
+
+--
+CREATE OR REPLACE PACKAGE BODY get_pwd
+AS
+   FUNCTION decrypt (KEY IN VARCHAR2, VALUE IN VARCHAR2)
+      RETURN VARCHAR2
+   AS
+      LANGUAGE JAVA
+      NAME 'oracle.apps.fnd.security.WebSessionManagerProc.decrypt(java.lang.String,java.lang.String) return java.lang.String';
+END get_pwd;
