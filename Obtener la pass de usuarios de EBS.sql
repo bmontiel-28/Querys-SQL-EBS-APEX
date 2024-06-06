d28aee9f79c3244ae432e5714620b489b33c8396
Brayan M <71532006+bmontiel-28@users.noreply.github.com>
2024-06-06 17:02:32 -0600

    Create Obtener la pass de usuarios de EBS.sql

---
+++ Obtener la pass de usuarios de EBS.sql
@@ -0,0 +1,27 @@
+SELECT
+    'USER:'||USR.USER_NAME
+   ,'PASS: '||GET_PWD.DECRYPT((
+        SELECT
+            (
+                SELECT
+                    GET_PWD.DECRYPT(FND_WEB_SEC.GET_GUEST_USERNAME_PWD,USERTABLE.ENCRYPTED_FOUNDATION_PASSWORD)
+                FROM
+                    DUAL
+            )AS APPS_PASSWORD
+        FROM
+            FND_USER USERTABLE
+        WHERE
+            USERTABLE.USER_NAME =(
+                SELECT
+                    SUBSTR(FND_WEB_SEC.GET_GUEST_USERNAME_PWD
+                          ,1
+                          ,INSTR(FND_WEB_SEC.GET_GUEST_USERNAME_PWD,'/')- 1)
+                FROM
+                    DUAL
+            )
+    )
+                    ,USR.ENCRYPTED_USER_PASSWORD)PASSWORD
+FROM
+    FND_USER USR
+WHERE
+    USR.USER_NAME IN ('MARTINEZJAI','MARQUEZV','DELEONV','LIMAD');
