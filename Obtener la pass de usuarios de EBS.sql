429b53d82106894ace062610e3a5290ed4f69945
Brayan M <71532006+bmontiel-28@users.noreply.github.com>
2024-06-07 10:20:47 -0600

    Update Obtener la pass de usuarios de EBS.sql

--- Obtener la pass de usuarios de EBS.sql
+++ Obtener la pass de usuarios de EBS.sql
@@ -1,6 +1,7 @@
 SELECT
-    'USER:'||USR.USER_NAME
-   ,'PASS: '||GET_PWD.DECRYPT((
+    'USER:' || USR.USER_NAME     AS USER_NAME
+   ,'PASS: '
+     || GET_PWD.DECRYPT((
         SELECT
             (
                 SELECT
@@ -20,29 +21,9 @@ SELECT
                     DUAL
             )
     )
-                    ,USR.ENCRYPTED_USER_PASSWORD)PASSWORD
+                       ,USR.ENCRYPTED_USER_PASSWORD) AS PASSWORD
+   ,'NAME: ' || UPPER(USR.DESCRIPTION)  AS USER_FULL_NAME
 FROM
     FND_USER USR
 WHERE
-    USR.USER_NAME IN ('MARTINEZJAI','MARQUEZV','DELEONV','LIMAD');
-
-
-
-
-
--- Paquete en caso de que no exista
-CREATE OR REPLACE PACKAGE get_pwd
-AS
-   FUNCTION decrypt (KEY IN VARCHAR2, VALUE IN VARCHAR2)
-      RETURN VARCHAR2;
-END get_pwd;
-
---
-CREATE OR REPLACE PACKAGE BODY get_pwd
-AS
-   FUNCTION decrypt (KEY IN VARCHAR2, VALUE IN VARCHAR2)
-      RETURN VARCHAR2
-   AS
-      LANGUAGE JAVA
-      NAME 'oracle.apps.fnd.security.WebSessionManagerProc.decrypt(java.lang.String,java.lang.String) return java.lang.String';
-END get_pwd;
+    USR.USER_NAME IN('MARTINEZJAI','MARQUEZV','DELEONV','LIMAD','GARCIANM');
